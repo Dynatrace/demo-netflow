c2c77c7f4fd077bc6456001741103035bd25f76e
Adam Gardner <adam.gardner@dynatrace.com>
2025-06-12 17:50:19 +1000

    update

--- docs/snippets/bizevent-resources.js
+++ docs/snippets/bizevent-resources.js
@@ -1,5 +1,5 @@
 <script>
 document.addEventListener('DOMContentLoaded', function() {
-  dynatrace.sendBizEvent('page_load', {"page": "7. resources"});
+  dynatrace.sendBizEvent('page_load', {"page": "6. resources"});
 });
 </script>
